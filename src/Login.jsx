0b38e104a55e57f93775f28e2491d4842f60ce2d
Muhammad <muhammadtahirbharchoond>
2023-05-16 23:07:19 +0500

    changes some files

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
     <div>
-      <h2>login page</h2>
+      <div className="mainContainer">
+        <div className="imageContainer">
+          <img
+            src="../src/assets/login.png"
+            alt="Login image for registration"
+            className="image"
+          />
+        </div>
+        <div className="formContainer">
+          <form action="submit-form" method="POST" onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              id="pasword"
+              pattern=".{8,}"
+              title="Password must be 8 characters"
+              placeholder="Password"
+              required
+            />
+            <button type="submit">Submit</button>
+            <div className="login">
+              <p>
+                If you are new create account <a href="">Register</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Registration.jsx
+++ src/Registration.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./registration.css";
-import {
-  Avatar,
-  Button,
-  Container,
-  CssBaseline,
-  Typography,
-} from "@mui/material";
-import { LockOutlined, Email, Lock, Person } from "@mui/icons-material";
 
 const Registration = () => {
   function handleSubmit(event) {
@@ -21,65 +13,44 @@ const Registration = () => {
         <div className="imageContainer">
           <img
             src="../src/assets/Rectangle 1.png"
-            alt="hiking image for registration"
+            alt="Rectangle image for registration"
             className="image"
           />
         </div>
         <div className="formContainer">
           <form action="submit-form" method="POST" onSubmit={handleSubmit}>
-            <div className="heading">
-              <Avatar>
-                <LockOutlined />
-              </Avatar>
-              <Typography component="h2" variant="h5">
-                Register
-              </Typography>
-            </div>
-            <div>
-              <Person />
-              <input
-                type="text"
-                name="firstName"
-                id="firstName"
-                placeholder="First name"
-                required
-              />
-            </div>
-            <div>
-              <Person />
-              <input
-                type="text"
-                name="lastName"
-                id="lastName"
-                placeholder="Last name"
-                required
-              />
-            </div>
-            <div>
-              <Email />
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-                required
-              />
-            </div>
-            <div>
-              <Lock />
-              <input
-                type="password"
-                name="password"
-                id="password"
-                pattern=".{8,}"
-                title="Password must be 8 characters"
-                placeholder="Password"
-                required
-              />
-            </div>
-            <button type="submit">
-              <Typography variant="button">Submit</Typography>
-            </button>
+            <h2>Register</h2>
+            <input
+              type="text"
+              name="first name"
+              id="first name"
+              placeholder="First name"
+              required
+            />
+            <input
+              type="text"
+              name="last name"
+              id="last name"
+              placeholder="last name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              id="pasword"
+              pattern=".{8,}"
+              title="Password must be 8 characters"
+              placeholder="Password"
+              required
+            />
+            <button type="submit">Submit</button>
             <div className="login">
               <p>
                 Already have an account? <Link to="/login">Login</Link>
