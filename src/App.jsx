236bdea0687c443d71d49a2bd796f7dc98c7b6f0
Muhammad <muhammadtahirbharchoond>
2023-05-16 22:47:52 +0500

    changes some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import Registration from './Registration.jsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './Login.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Registration from './Registration';
+import Login from './Login';
+
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path='/' Component={Registration}/>
-        <Route path='/login' Component={Login}/>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
